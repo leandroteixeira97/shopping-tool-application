858b50827c6592ab50959e456a3a282d3f51c0b3
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-12-06 21:42:40 -0300

    `feat:` Initializing the implementation of routes

---
+++ src/pages/AuthenticationPage/AuthenticationPage.tsx
@@ -0,0 +1,7 @@
+export function AuthenticationPage(): JSX.Element {
+  return (
+    <>
+      <h1>LOGIN</h1>
+    </>
+  );
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+export function Home(): JSX.Element {
+  return (
+    <>
+      <h1>HOME</h1>
+    </>
+  );
+}
